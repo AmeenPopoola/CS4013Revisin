a2d286094918824952594ef79c255d30958d709d
AmeenPopoola <82396792+AmeenPopoola@users.noreply.github.com>
2021-09-17 09:22:38 +0100

    Add files via upload

---
+++ Exercise1_5.java
@@ -0,0 +1,12 @@
+
+
+public class Exercise1_5 {
+    public static void main(String[]args){
+     double numerator = (7.5 * 2.5) - (1.5 * 3) ;
+     double denominator = 53.5 -2.5;
+     
+     double result = numerator/ denominator;
+     
+     System.out.println(result);
+    }
+}
