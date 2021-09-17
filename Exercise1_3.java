d0af915fdd10a520077dd581aa2024df7e6b0bd0
AmeenPopoola <82396792+AmeenPopoola@users.noreply.github.com>
2021-09-17 09:22:06 +0100

    Add files via upload

---
+++ Exercise1_3.java
@@ -0,0 +1,18 @@
+
+
+public class Exercise1_3 {
+    public static void main (String [] args) {
+       System.out.println("    ]     A    V     V    A");
+       System.out.println("    ]    A A    V   V    A A");
+       System.out.println("]   ]   AAAAA    V V    AAAAA");
+       System.out.println(" ] ]   A     A    V    A     A");
+        
+        
+        
+        
+        
+        
+        
+    }
+}
+  
